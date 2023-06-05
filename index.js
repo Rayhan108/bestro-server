5ffaa32173f8923793e45ff67e4aac13c9793a4d
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-06 00:04:23 +0600

    verify jWt token

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ app.use(cors());
 const jwt = require('jsonwebtoken');
 app.use(express.json());
 
+const verifyJWT=(req,res,next)=>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'});
+    // bearer token
+    const token=authorization.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({error:true,message:'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
+}
 app.get("/", (req, res) => {
   res.send("Bestro bosss server is running");
 });
@@ -85,10 +100,15 @@ app.post('/jwt',(req,res)=>{
       res.send(result);
     });
     //  cart collection api
-    app.get("/carts", async (req, res) => {
+    app.get("/carts",verifyJWT ,async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+
+      if(email !== decodedEmail){
+        return res.status(403).send({error:true,message:'Forbiden access'});
       }
       const query = { email: email };
       const result = await cartsCollection.find(query).toArray();
